632c3910da7af9be627902dd2e4c6cfa54f600ca
resurtm <resurtm@gmail.com>
2017-02-16 16:26:49 +0600

    rework

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     name: 'shpng',
     target: 'web',
     entry: [
+        path.join(__dirname, 'frontend', 'main.scss'),
         path.join(__dirname, 'frontend', 'main.jsx')
     ],
     output: {
@@ -21,15 +21,12 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                loader: ExtractTextPlugin.extract(['style-loader', 'css-loader', 'sass-loader'])
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
             },
             {
                 test: /\.(jpg|jpeg|png|gif|woff|woff2|eot|ttf|svg)(.*)$/,
                 loader: 'url-loader?limit=5000'
             }
         ]
-    },
-    plugins: [
-        new ExtractTextPlugin('main.css')
-    ]
+    }
 };
